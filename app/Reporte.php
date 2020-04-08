aa5231a5e83a78f7f5c9486355883901cd4192aa
RubenJSP <56006571+RubenJSP@users.noreply.github.com>
2020-04-07 18:19:30 -0600

    FIXES

--- app/Reporte.php
+++ app/Reporte.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reporte extends Model
 {
-    
+    protected $table = 'reporte';
      protected $fillable = [
-        'idUsuario','idJuego','mensaje'
+        'idUsuario','juego','mensaje'
     ];
     
 }
