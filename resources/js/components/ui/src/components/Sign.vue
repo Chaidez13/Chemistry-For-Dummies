70333d94ca735af16f1988da140c2dd14175fd37
Chaidez13 <56051074+Chaidez13@users.noreply.github.com>
2020-05-02 22:52:13 -0600

    m

--- resources/js/components/ui/src/components/Sign.vue
+++ resources/js/components/ui/src/components/Sign.vue
@@ -149,8 +149,6 @@ export default {
          }
        }).then(() => {
          this.validarSesion()
-         window.location.href = '/'
-         location.reload();
        }).catch((e) =>{
          const errorCode = e.response.status
          if(errorCode == 422){
